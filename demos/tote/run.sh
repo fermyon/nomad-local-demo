8ccbbf5abec55ab66549a7b11b8720381542f262
Adam Reese <adam@reese.io>
2022-04-07 10:43:03 -0700

    Move tote to demos

---
+++ demos/tote/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export VAULT_ADDR='http://localhost:8200'
+export VAULT_TOKEN='devroot'
+export BINDLE_URL='http://bindle.local.fermyon.link:8088/v1'
+
+require() {
+  if ! hash "$1" &>/dev/null; then
+    echo "'$1' not found in PATH"
+    exit 1
+  fi
+}
+
+require hippo
+
+vault secrets enable kv 2>/dev/null || :
+vault kv put kv/tote foo=bar
+
+cargo build-wasm --release
+hippo bindle -v production HIPPOFACTS
+
+echo "Starting tote job..."
+nomad run job/tote.nomad
